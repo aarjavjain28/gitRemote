58b3715e6fa71d4ea30a379a0ac8fd1abd3a3624
Aarjav Jain <aarjav.jain28@thehutgroup.com>
2022-09-27 14:39:20 +0100

    added jupiter

--- hello.py
+++ hello.py
@@ -1,4 +1,18 @@
-def hello():
+import sys
+
+
+def helloworld():
 	print('hello, world')
+def hellojupiter():
+	print('hello jupiter')
+
+def hello():
+	if sys.argv[1] =='Jupiter':
+		hellojupiter()
+	else:
+		helloworld()
+		
+
+
 if __name__ == '__main__':
 	hello()
