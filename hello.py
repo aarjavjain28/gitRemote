3d82af52691d279946fac6c586c8aa442728c56b
Aarjav Jain <aarjav.jain28@thehutgroup.com>
2022-09-27 14:23:10 +0100

    added mars feature

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
+import sys
+
 def hello():
+	if sys.argv[1] == 'mars':
+		hellomars()
+	else:
+		helloworld()
+
+def hellomars():
+	print('hello, mars')
+
+def helloworld():
 	print('hello, world')
 if __name__ == '__main__':
 	hello()
